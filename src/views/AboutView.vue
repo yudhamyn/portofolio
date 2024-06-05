eaa5edca952b53e1d265aeb78b8a40120b67f932
Yudhamyn <101394691+yudhamyn@users.noreply.github.com>
2024-06-05 13:08:56 +0700

    Init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,277 @@
+<script>
+export default {
+  data() {
+    return {
+      activeTab: 1,
+      tech: [
+        {
+          id: 1,
+          name: 'Python',
+          imageUrl: 'https://cdn.iconscout.com/icon/free/png-512/free-python-2-226051.png?f=webp&w=512',
+          status: 'Advanced'
+        },
+        {
+          id: 2,
+          name: 'Machine Learn',
+          imageUrl: 'https://cdn.iconscout.com/icon/premium/png-512-thumb/computational-intelligence-2040740-1721091.png?f=webp&w=512',
+          status: 'Advanced'
+        },
+        {
+          id: 3,
+          name: 'MySQL',
+          imageUrl: 'https://cdn.iconscout.com/icon/free/png-512/free-mysql-3628940-3030165.png?f=webp&w=512',
+          status: 'Advanced'
+        },
+        {
+          id: 4,
+          name: 'HTML',
+          imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/html-2752158-2284975.png',
+          status: 'Advance'
+        },
+        {
+          id: 5,
+          name: 'PHP',
+          imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-php-2038871-1720084.png',
+          status: 'Advanced'
+        },
+        {
+          id: 6,
+          name: 'django',
+          imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/django-1-282754.png',
+          status: 'Intermediate'
+        },
+        {
+          id: 7,
+          name: 'Laravel',
+          imageUrl: 'https://cdn.worldvectorlogo.com/logos/laravel-2.svg',
+          status: 'Intermediate'
+        },
+        {
+          id: 8,
+          name: 'Codeigniter',
+          imageUrl: 'https://cdn-peanutsquare.b-cdn.net/wp-content/uploads/2022/11/codeigniter-logo.png',
+          status: 'Intermediate'
+        },
+        {
+          id: 9,
+          name: 'Bootstrap',
+          imageUrl: 'https://getbootstrap.com/docs/5.2/assets/brand/bootstrap-logo-shadow.png',
+          status: 'Intermediate'
+        },
+        {
+          id: 10,
+          name: 'css',
+          imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/css-131-722685.png',
+          status: 'Intermediate'
+        },
+        {
+          id: 11,
+          name: 'tailwind',
+          imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1024px-Tailwind_CSS_Logo.svg.png',
+          status: 'Intermediate'
+        },
+        {
+          id: 10,
+          name: 'sass',
+          imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/sass-226054.png',
+          status: 'Intermediate'
+        },
+
+      ],
+      tools: [
+        {
+          id: 1,
+          name: 'Git',
+          imageUrl: 'https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png',
+          status: 'Version Control'
+        },
+        {
+          id: 2,
+          name: 'GitHub',
+          imageUrl: 'https://cdn-icons-png.flaticon.com/512/25/25231.png',
+          status: 'Git Hosting'
+        },
+        {
+          id: 3,
+          name: 'pip',
+          imageUrl: 'https://cdn-icons-png.flaticon.com/512/1822/1822899.png',
+          status: 'Package Manager'
+        },
+        {
+          id: 4,
+          name: 'jupyter notebook',
+          imageUrl: 'https://raw.githubusercontent.com/github/explore/a4691f04ff219c1c2aa02fc61fda41aa43f1459a/topics/jupyter-notebook/jupyter-notebook.png',
+          status: 'IDE'
+        },
+        {
+          id: 5,
+          name: 'VS Code',
+          imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/visual-studio-code-1868941-1583105.png',
+          status: 'IDE'
+        },
+        {
+          id: 6,
+          name: 'Colab',
+          imageUrl: 'https://avatars.githubusercontent.com/u/38081706?v=4',
+          status: 'IDE'
+        },
+        {
+          id: 7,
+          name: 'Google Looker',
+          imageUrl: 'https://avatars.githubusercontent.com/u/46035403?s=280&v=4',
+          status: 'BI Tool'
+        },
+        {
+          id: 8,
+          name: 'Tableau',
+          imageUrl: 'https://cdn.filepicker.io/api/file/jZDILlufSOSDOkuJTZ7J',
+          status: 'BI Tool'
+        },
+      ]
+    };
+  }
+}
+</script>
+<template>
+  <div
+    class="bg-[#1e1e1f] px-5 py-5 md:px-12 md:py-10 text-left border border-[#383838] rounded-3xl text-amber-50 mx-3 mb-5">
+    <article data-page="about">
+
+      <header>
+        <div class="text-2xl font-bold text-white mb-5 fadein-bot title-section flex items-center">
+          About Me &nbsp;
+          <div class="h-[1px] w-32 bg-blue-200 md:w-96 aos-init aos-animate" data-aos="zoom-in-left"
+            data-aos-duration="600"></div>
+        </div>
+      </header>
+
+      <section
+        class="text-sm md:text-lg text-justify flex flex-col gap-4 md:flex-row md:gap-8 md:justify-left md:items-center">
+        <div class="flex justify-center">
+          <img class="w-9/12 rounded-full mb-3 fadein-up"
+            src="/img/yudhaformal.jpg" alt="Foto">
+        </div>
+        <div class="md:w-7/12">
+          <p class="mb-3 md:mb-7 fadein-left fadeins-1">
+            &nbsp; &nbsp; &nbsp; Hi everyone! My name is Yudha Mulyana. I'm a Data enthusiast with a background in technology information system, specializing in research and spatial data analysis. Transitioned from 2 years in course program management and project based to pursue a career in data analytics. 
+Skilled in research, analytical thinking, and project management, providing a solid foundation for work as a data scientist and analyst. Proficient in spreadsheets, Python, SQL, BI, and GIS for extracting insights from complex datasets. Dedicated to continuous learning and eager to stay updated with the latest trends and techniques in data analysis. Committed to leveraging data to drive informed decision-making processes.
+          </p>
+        </div>
+
+      </section>
+
+    </article>
+  </div>
+
+  <div class="px-5 py-5 md:px-12 md:py-10 text-left text-amber-50 mx-3">
+    <article data-page="about">
+      <header>
+        <div class="text-2xl font-bold text-white mb-5 fadein-bot title-section flex items-center">
+          <div class="h-[1px] w-10 bg-blue-200 md:w-20 aos-init aos-animate" data-aos="zoom-in-left"
+            data-aos-duration="600"></div>
+          &nbsp; Skills
+        </div>
+      </header>
+      <section>
+        <div>
+          <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 mb-5">
+            <li class="mr-2">
+              <button class="inline-block px-4 py-3 rounded-lg hover:text-white"
+                :class="{ 'text-blue-200 bg-blue-200 bg-opacity-10': activeTab === 1 }" @click="activeTab = 1">Tech Stack</button>
+            </li>
+            <li class="mr-2">
+              <button class="inline-block px-4 py-3 rounded-lg hover:text-white"
+                :class="{ 'text-blue-200 bg-blue-200 bg-opacity-10': activeTab === 2 }" @click="activeTab = 2">Tools</button>
+            </li>
+          </ul>
+        </div>
+        <div v-show="activeTab === 1">
+          <div class="grid grid-cols-2 gap-4 pb-32 md:grid-cols-3 md:gap-8 xl:grid-cols-4 xl:gap-10 2xl:gap-12">
+            <div v-for="item in tech" :key="item.id">
+              <div
+                class="item-tech flex cursor-pointer items-center gap-2 rounded border border-blue-200 px-2 py-2 hover:bg-blue-200 hover:bg-opacity-10 md:gap-3 lg:px-3">
+                <div class="flex h-12 w-12 items-center justify-center p-0 lg:h-16 lg:w-16 lg:p-2 zoom-in">
+                  <img alt="HTML" loading="lazy" width="32" height="32" decoding="async" data-nimg="1"
+                    class="img-tech drop-shadow-xl transition-all duration-300 h-[65%] w-[65%] lg:h-[85%] lg:w-[85%]  "
+                    :src="item.imageUrl" style="color: transparent;">
+                </div>
+                <div class="flex items-center text-sm md:text-base lg:text-lg">
+                  <div class="tech font-medium text-secondary transition-all duration-300 translate-y-0 ">{{ item.name }}
+                  </div>
+                  <div
+                    class="status-tech opacity-0 absolute mt-5 text-[10px] text-blue-200 transition-all duration-300 md:text-xs lg:text-sm">
+                    {{ item.status }}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div v-show="activeTab === 2">
+          <div class="grid grid-cols-2 gap-4 pb-32 md:grid-cols-3 md:gap-8 xl:grid-cols-4 xl:gap-10 2xl:gap-12">
+            <div v-for="item in tools" :key="item.id">
+              <div
+                class="item-tech flex cursor-pointer items-center gap-2 rounded border border-blue-200 px-2 py-2 hover:bg-blue-200 hover:bg-opacity-10 md:gap-3 lg:px-3">
+                <div class="flex h-12 w-12 items-center justify-center p-0 lg:h-16 lg:w-16 lg:p-2 zoom-in">
+                  <img alt="HTML" loading="lazy" width="32" height="32" decoding="async" data-nimg="1"
+                    class="img-tech drop-shadow-xl transition-all duration-300 h-[65%] w-[65%] lg:h-[85%] lg:w-[85%]  "
+                    :src="item.imageUrl" style="color: transparent;">
+                </div>
+                <div class="flex items-center text-sm md:text-base lg:text-lg">
+                  <div class="tech font-medium text-secondary transition-all duration-300 translate-y-0 ">{{ item.name }}
+                  </div>
+                  <div
+                    class="status-tech opacity-0 absolute mt-5 text-[10px] text-blue-200 transition-all duration-300 md:text-xs lg:text-sm">
+                    {{ item.status }}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </article>
+  </div>
+</template>
+
+<style>
+.fadein-left {
+  opacity: 0;
+  animation: fadeInLeft 0.5s ease-out forwards;
+}
+
+@keyframes fadeInLeft {
+  0% {
+    opacity: 0;
+    transform: translateX(100%);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+.fadeins-1 {
+  animation-delay: 500ms;
+}
+
+.fadeins-2 {
+  animation-delay: 800ms;
+}
+
+.img-tech,
+.tech {
+  transition: transform 0.3s ease;
+}
+
+.item-tech:hover .img-tech {
+  transform: scale(1.3);
+}
+
+.item-tech:hover .tech {
+  transform: translateY(-12px);
+}
+
+.item-tech:hover .status-tech {
+  opacity: 1;
+}
+</style>
---
+++ src/views/PortfolioView.vue
@@ -0,0 +1,194 @@
+<template>
+  <div class="px-5 py-5 md:px-12 md:py-10 text-left text-amber-50 mx-3">
+    <article data-page="about">
+      <header>
+        <div
+          class="text-2xl font-bold text-white mb-10 fadein-bot title-section flex items-center justify-center flex-col">
+          <h4>Past Project Experience</h4>
+          <h4 class="text-base font-normal text-transparent bg-clip-text bg-gradient-to-r from-slate-100 to-blue-300">
+            Explore the projects I've worked on so far</h4>
+        </div>
+      </header>
+      <section>
+        <div>
+          <div class="grid grid-cols-1 gap-4 pb-32 md:grid-cols-3 md:gap-3 xl:grid-cols-3 xl:gap-3 2xl:gap-5 fade-zoom-in">
+            <div v-for="item in items" :key="item.id">
+              <div
+                class="item-card flex flex-col items-center gap-2 rounded bg-[#1e1e1f] hover:bg-[#282828] border border-[#383838] rounded-xl text-amber-50 md:gap-3 px-5 py-5 lg:px-5 ">
+                <div class="flex h-12 w-12 items-center justify-center p-0 h-full w-full lg:p-0 zoom-in">
+                  <img alt="HTML" loading="lazy" decoding="async" data-nimg="1" class="drop-shadow-xl rounded rounded-xl"
+                    :src="getImageUrl(item.imageUrl)">
+                </div>
+                <div class="w-full flex flex-col gap-2 items-center text-sm md:text-base lg:text-lg">
+                  <div class="title-text font-medium text-secondary">{{ item.name }}
+                  </div>
+                  <div class="w-full text-left text-[10px] text-[#c1c1c1] md:text-xs lg:text-sm">
+                    {{ item.status }}</div>
+                  <div class="w-full mt-4 text-normal text-sm text-left text-blue-200">
+                    {{ item.tech }}
+                  </div>
+                  <div class="w-full flex justify-end">
+                    <div class="flex cursor-pointer items-end gap-2 text-primary">
+                      <a v-if="item.github !== 'null'"
+                        :href="item.github" target="_blank" rel="noreferrer"
+                        title="View github repository" class="transition-all hover:text-accent">
+                        <svg stroke="currentColor"
+                          fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
+                          height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                          <path
+                            d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
+                          </path>
+                        </svg></a>
+                        <a v-if="item.demo !== 'null'" :href="item.demo" target="_blank" rel="noreferrer"
+                        title="View finished project" class="transition-all hover:text-accent">
+                        <svg stroke="currentColor"
+                          fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
+                          height="18" width="18" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                          <polyline points="15 3 21 3 21 9"></polyline>
+                        <line x1="10" y1="14" x2="21" y2="3"></line>
+                      </svg></a></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </article>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeTab: 1,
+      items: [
+        {
+          id: 1,
+          name: 'Clustering Global Air Pollution Data',
+          imageUrl: 'clustering',
+          status: 'Project ini bertujuan untuk menganalisis data polusi udara global dan mengelompokkannya ke dalam klaster berdasarkan karakteristik polusi udara menggunakan metode klasterisasi. Data yang digunakan mencakup berbagai parameter polusi udara seperti AQI, konsentrasi CO, ozon, NO2, dan PM2.5. Setelah melakukan eksplorasi data, analisis korelasi, dan pemrosesan data, klasterisasi dilakukan dengan menggunakan metode KMeans dan memanfaatkan reduksi dimensi dengan PCA. Hasilnya, ditemukan bahwa sebagian besar kota memiliki kualitas udara yang baik, namun ada variasi yang signifikan antar negara dan kota. Dengan melakukan klasterisasi dan membuat dashboard visualisasinya.',
+          tech: 'ipynb, Google Looker Studio',
+          github: 'https://github.com/yudhamyn/Global-Air-Pollution-Clustering',
+          demo: 'https://lookerstudio.google.com/reporting/d33e3fa1-4b6b-408c-8e57-624eaf4f93ef'
+        },
+        {
+          id: 2,
+          name: 'Diabets Clasification',
+          imageUrl: 'diabets',
+          status: 'Saya membangun model prediksi diabetes menggunakan dataset dari National Institute of Diabetes and Digestive and Kidney Diseases, dengan tujuan memprediksi apakah seorang pasien memiliki diabetes berdasarkan beberapa pengukuran diagnostik. Setelah menangani nilai hilang, outlier, dan rekayasa fitur, saya menguji beberapa model (termasuk Logistic Regression, KNN, Decision Tree, Random Forest, SVC, XGBoost, Gradient Boosting, dan LightGBM) menggunakan cross-validation. Model terbaik berdasarkan grafik evaluasi adalah LightGBM, yang mencapai akurasi, presisi, recall, dan F1 score yang tinggi pada data uji, dengan akurasi sebesar 90.90%, presisi 83.13%, recall 92.59%, dan F1 score 87.71% pada data uji. Model LightGBM ini memiliki performa yang baik dalam memprediksi diabetes berdasarkan beberapa pengukuran diagnostik pada pasien.',
+          tech: 'ipynb',
+          github: 'https://github.com/yudhamyn/Diabetes-Prediction-Classification--Data-Science-Project-6-GreatEdu',
+          demo: 'null'
+        },
+        {
+          id: 3,
+          name: 'RFM Online Retail',
+          imageUrl: 'RFMonlineretail',
+          status: 'Projek ini bertujuan untuk melakukan segmentasi pelanggan berdasarkan perilaku pembelian menggunakan metode RFM (Recency, Frequency, Monetary) analysis. Melalui eksplorasi data dan analisis clustering, ditemukan bahwa terdapat variasi dalam pola pembelian dan preferensi pelanggan. Segmentasi ini memungkinkan perusahaan untuk memahami lebih dalam tentang perilaku pelanggan dan menyusun strategi pemasaran yang lebih efektif, seperti program loyalitas yang disesuaikan dan penawaran produk yang lebih tepat sasaran. Selain itu, analisis segmentasi juga memberikan wawasan tentang karakteristik demografis pelanggan, seperti perbedaan preferensi produk berdasarkan faktor demografis. Dengan demikian, proyek ini memberikan kontribusi dalam meningkatkan pemahaman tentang pelanggan dan mengoptimalkan strategi pemasaran untuk mencapai keberhasilan bisnis yang lebih baik.',
+          tech: 'ipynb',
+          github: 'https://github.com/yudhamyn/Clustering-Online-Retail-RFM-Data-Science-Project-V-GreatEdu',
+          demo: 'null'
+        },
+        {
+          id: 4,
+          name: 'Project Data Scientist Home Credit Indonesia',
+          imageUrl: 'HomeCredit',
+          status: 'Projek ini bertujuan untuk memprediksi kemampuan pembayaran klien pada data aplikasi pinjaman dengan menggunakan berbagai model machine learning seperti Logistic Regression, Gaussian Naive Bayes, Decision Tree, Random Forest, K-Nearest Neighbors, dan Neural Network. Proses dimulai dengan mempersiapkan data, termasuk menangani nilai yang hilang, mengkodekan fitur kategorikal, dan mengonversi nilai negatif menjadi nilai positif. Setelah itu, berbagai model ML dilatih dan dievaluasi menggunakan metrik akurasi, serta dianalisis fitur pentingnya. Hasilnya menunjukkan bahwa model Random Forest memberikan kinerja terbaik dengan akurasi prediksi yang tinggi. Dengan menggunakan model ini, proyek ini dapat memberikan rekomendasi kepada penyedia pinjaman untuk menentukan strategi pemasaran yang lebih efektif dan memberikan wawasan tentang faktor-faktor yang mempengaruhi kemampuan pembayaran klien.',
+          tech: 'ipynb, PowerPoint',
+          github: 'https://github.com/yudhamyn/HOME-CREDIT-Final-Task-Data-Scientist',
+          demo: 'https://github.com/yudhamyn/HOME-CREDIT-Final-Task-Data-Scientist/blob/main/Final%20Task%20Yudha%20mulyana%20Home%20Credit%20Data%20Science.pdf'
+        },
+        {
+          id: 5,
+          name: 'Project Data Scientist Kalbe Nutritionals',
+          imageUrl: 'kalbe',
+          status: 'Project ini merupakan sebuah analisis data yang bertujuan untuk mengoptimalkan strategi pemasaran dan meningkatkan pengalaman pelanggan pada sebuah perusahaan ritel. Proses dimulai dengan penggabungan beberapa dataset yang mencakup informasi transaksi pelanggan, produk, dan toko. Selanjutnya, dilakukan berbagai tahap preprocessing data, seperti pembersihan data, penyesuaian tipe data, dan pengecekan stasioneritas data menggunakan uji Augmented Dickey-Fuller. Selain itu, dilakukan analisis regresi time series untuk meramalkan jumlah penjualan. Hasilnya menunjukkan bahwa model ARIMA dengan parameter manual (40,2,2) memberikan performa terbaik. Selain itu, dilakukan analisis clustering menggunakan algoritma K-Means untuk mengidentifikasi pola perilaku pelanggan. Dari hasil clustering tersebut, ditemukan empat kelompok pelanggan dengan karakteristik yang berbeda, yang kemudian diikuti dengan rekomendasi strategi pemasaran yang sesuai untuk setiap kelompok pelanggan guna meningkatkan retensi pelanggan dan penjualan erta membuat dashboard visualisasi penjualan menggunakan Tableau.',
+          tech: 'ipynb, Tableau',
+          github: 'https://github.com/rakha-elctrnx/muslim-companion',
+          demo: 'https://github.com/yudhamyn/FinalTask-Kalbe-DataScientist-YudhaMulyana/blob/main/Kalbe%20Data%20Scientist.twbx'
+        },
+        {
+          id: 6,
+          name: 'NLP | Deteksi Sentimen Positif Negatif Bahasa Indonesia',
+          imageUrl: 'deteksipositifnegatif',
+          status: 'Proyek ini bertujuan untuk melakukan analisis sentimen pada teks dalam bahasa Indonesia menggunakan model BERT. Pertama, model BERT untuk klasifikasi urutan teks dipersiapkan dengan tokenisasi menggunakan tokenizer Bahasa Indonesia dan konfigurasi yang sesuai. Model ini kemudian dilatih menggunakan dataset SMSA Doc Sentiment Prosa yang telah diproses sebelumnya. Setelah melatih model, dilakukan evaluasi menggunakan dataset validasi dan testing. Hasilnya menunjukkan bahwa model yang dilatih mampu mencapai akurasi yang baik dalam mengklasifikasikan sentimen pada teks Bahasa Indonesia apakah positif atau negatif.',
+          tech: 'ipynb, pytorch, transformers',
+          github: 'https://github.com/rakha-elctrnx/muslim-companion',
+          demo: 'null'
+        },
+        {
+          id: 7,
+          name: 'Clustering Alasan Resign Karyawan',
+          imageUrl: 'clusterkryawan',
+          status: 'Project ini merupakan analisis data terhadap pola-pola yang mungkin terjadi pada karyawan yang mengundurkan diri dari perusahaan. Proses dimulai dengan melakukan preprocessing data, termasuk mengelompokkan alasan resign menjadi kategori-kategori tertentu, mengisi nilai yang hilang, dan mengubah variabel kategorikal menjadi representasi numerik. Selanjutnya, dilakukan analisis clustering menggunakan algoritma K-Means untuk mengelompokkan karyawan berdasarkan pola-pola yang teridentifikasi. Evaluasi dilakukan menggunakan metode Elbow untuk menentukan jumlah klaster yang optimal, serta pengukuran inertia dan silhouette score untuk mengevaluasi performa klasterisasi. Hasil klasterisasi divisualisasikan menggunakan scatter plot dua dimensi untuk memudahkan interpretasi.',
+          tech: 'ipynb, PowerPoint',
+          github: 'https://github.com/rakha-elctrnx/muslim-companion',
+          demo: 'https://github.com/yudhamyn/TugasAkhirDataMining_Clustering_Alasan-Resign-Karyawan-Menggunakan-DataPrivate/blob/main/GUI%20STREAMLIT_PPT%20CRISP-DM_TUGAS%20AKHIR_YUDHA%20MULYANA_A11.2021.13857_442U/PPT%20Resign.pdf'
+        },
+        {
+          id: 7,
+          name: 'Prediksi Kelulusan Mahasiswa',
+          imageUrl: 'prediksikelulusan',
+          status: 'Proyek ini merupakan penggunaan algoritma K-Nearest Neighbors (KNN) untuk memprediksi apakah seorang mahasiswa akan lulus tepat waktu berdasarkan nilai indeks prestasi (IP) mereka. Data tersebut telah disiapkan dan dibersihkan dengan mengubah label "Ya" dan "Tidak" pada kolom "tepat" menjadi nilai biner (-1 untuk "Tidak" dan 1 untuk "Ya"). Setelah membagi data menjadi data latih dan data uji, model KNN dilatih dengan menggunakan data latih dan kemudian diuji dengan data uji. Hasil akurasi model mencapai sekitar 87.32%. Pengguna juga dapat memasukkan nilai IP baru untuk melakukan prediksi apakah mahasiswa tersebut akan lulus tepat waktu.',
+          tech: 'ipynb',
+          github: 'https://github.com/yudhamyn/DataMining_klasifikasi-kelulusan_tepat-atau-tidak/blob/main/Yudha_Prediksi_lulus_tepat_waktu.ipynb',
+          demo: 'null'
+        },
+        {
+          id: 8,
+          name: 'Bank Churn Prediction',
+          imageUrl: 'Bankchurn',
+          status: 'Setelah melakukan feature engineering dan encoding data untuk meningkatkan kualitas dan representasi fitur dalam prediksi churn pelanggan bank, berbagai model machine learning diuji untuk kinerja terbaik. Model-model yang diuji meliputi Logistic Regression, Random Forest, Gradient Boosting, XGBoost, dan LightGBM. Setelah menerapkan teknik tuning hyperparameter yang komprehensif, model XGBoost menunjukkan performa terbaik dengan akurasi mencapai 87% dan recall 52% untuk kelas positif, menunjukkan kemampuannya dalam mengidentifikasi pelanggan yang mungkin churn. Model Random Forest dan LightGBM juga memberikan hasil kompetitif dengan akurasi masing-masing sekitar 86% dan 87%, meskipun kombinasi precision dan recall tidak seoptimal XGBoost. Model Gradient Boosting dan Logistic Regression memiliki performa sedikit lebih rendah dibandingkan yang lainnya. Dalam hal keseimbangan antara precision dan recall, tuned XGBoost diidentifikasi sebagai model paling efektif untuk memprediksi churn dalam dataset ini, karena mampu memberikan prediksi yang lebih akurat dan andal, menjadikannya pilihan terbaik untuk diimplementasikan dalam strategi mitigasi churn di bank.',
+          tech: 'ipynb',
+          github: 'https://github.com/yudhamyn/Banking-Churn-Prediction-Data-Science-Project-IV-GreatEdu/blob/main/Tugas_Mandiri_Data_Science_Project_4_Yudha_Mulyana.ipynb',
+          demo: 'null'
+        },
+      ]
+    };
+  },
+  methods: {
+    getImageUrl(imageUrl) {
+      const supportedFormats = ['jpg', 'png', 'gif'];
+      for (let format of supportedFormats) {
+        let fullPath = `/img/${imageUrl}.${format}`;
+        if (this.imageExists(fullPath)) {
+          return fullPath;
+        }
+      }
+      return ''; // or a default image path if no image is found
+    },
+    imageExists(url) {
+      const http = new XMLHttpRequest();
+      http.open('HEAD', url, false);
+      http.send();
+      return http.status !== 404;
+    }
+  }
+}
+</script>
+
+<style>
+.item-card:hover {
+  transition: transform 0.3s ease;
+  transform: translateY(-8px);
+}
+svg:hover{
+  stroke: #00f9ff;
+}
+@keyframes fadeZoomIn {
+  0% {
+    opacity: 0;
+    transform: scale(0.5);
+  }
+   100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+.fade-zoom-in {
+  animation: fadeZoomIn 1s ease-in-out;
+}
+</style>
