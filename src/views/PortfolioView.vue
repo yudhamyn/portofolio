01bfe9751c8866fdfe7e2d3fae94a4cbd1a85476
Yudhamyn <101394691+yudhamyn@users.noreply.github.com>
2024-06-08 17:46:28 +0700

    Update HomeView.vue

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -2,61 +2,85 @@
   <div class="px-5 py-5 md:px-12 md:py-10 text-left text-amber-50 mx-3">
     <article data-page="about">
       <header>
-        <div
-          class="text-2xl font-bold text-white mb-10 fadein-bot title-section flex items-center justify-center flex-col">
+        <div class="text-2xl font-bold text-white mb-10 fadein-bot title-section flex items-center justify-center flex-col">
           <h4>Past Project Experience</h4>
           <h4 class="text-base font-normal text-transparent bg-clip-text bg-gradient-to-r from-slate-100 to-blue-300">
             Explore the projects I've worked on so far</h4>
         </div>
+        <div class="tabs flex justify-center gap-4 mb-8">
+          <button 
+            @click="activeTab = 1"
+            :class="{'text-white border-b-2 border-blue-300': activeTab === 1}">
+            Projects
+          </button>
+          <button 
+            @click="activeTab = 2"
+            :class="{'text-white border-b-2 border-blue-300': activeTab === 2}">
+            Certificates
+          </button>
+        </div>
       </header>
       <section>
-        <div>
+        <div v-if="activeTab === 1">
           <div class="grid grid-cols-1 gap-4 pb-32 md:grid-cols-3 md:gap-3 xl:grid-cols-3 xl:gap-3 2xl:gap-5 fade-zoom-in">
             <div v-for="item in items" :key="item.id">
-              <div
-                class="item-card flex flex-col items-center gap-2 rounded bg-[#1e1e1f] hover:bg-[#282828] border border-[#383838] rounded-xl text-amber-50 md:gap-3 px-5 py-5 lg:px-5 ">
+              <div class="item-card flex flex-col items-center gap-2 rounded bg-[#1e1e1f] hover:bg-[#282828] border border-[#383838] rounded-xl text-amber-50 md:gap-3 px-5 py-5 lg:px-5 ">
                 <div class="flex h-12 w-12 items-center justify-center p-0 h-full w-full lg:p-0 zoom-in">
                   <img alt="HTML" loading="lazy" decoding="async" data-nimg="1" class="drop-shadow-xl rounded rounded-xl"
                     :src="getImageUrl(item.imageUrl)">
                 </div>
                 <div class="w-full flex flex-col gap-2 items-center text-sm md:text-base lg:text-lg">
-                  <div class="title-text font-medium text-secondary">{{ item.name }}
-                  </div>
-                  <div class="w-full text-left text-[10px] text-[#c1c1c1] md:text-xs lg:text-sm">
-                    {{ item.status }}</div>
-                  <div class="w-full mt-4 text-normal text-sm text-left text-blue-200">
-                    {{ item.tech }}
-                  </div>
+                  <div class="title-text font-medium text-secondary">{{ item.name }}</div>
+                  <div class="w-full text-left text-[10px] text-[#c1c1c1] md:text-xs lg:text-sm">{{ item.status }}</div>
+                  <div class="w-full mt-4 text-normal text-sm text-left text-blue-200">{{ item.tech }}</div>
                   <div class="w-full flex justify-end">
                     <div class="flex cursor-pointer items-end gap-2 text-primary">
-                      <a v-if="item.github !== 'null'"
-                        :href="item.github" target="_blank" rel="noreferrer"
-                        title="View github repository" class="transition-all hover:text-accent">
-                        <svg stroke="currentColor"
-                          fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
-                          height="16" width="16" xmlns="http://www.w3.org/2000/svg">
-                          <path
-                            d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
-                          </path>
-                        </svg></a>
-                        <a v-if="item.demo !== 'null'" :href="item.demo" target="_blank" rel="noreferrer"
-                        title="View finished project" class="transition-all hover:text-accent">
-                        <svg stroke="currentColor"
-                          fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
-                          height="18" width="18" xmlns="http://www.w3.org/2000/svg">
+                      <a v-if="item.github !== 'null'" :href="item.github" target="_blank" rel="noreferrer" title="View github repository" class="transition-all hover:text-accent">
+                        <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                        </svg>
+                      </a>
+                      <a v-if="item.demo !== 'null'" :href="item.demo" target="_blank" rel="noreferrer" title="View finished project" class="transition-all hover:text-accent">
+                        <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="18" width="18" xmlns="http://www.w3.org/2000/svg">
                           <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                           <polyline points="15 3 21 3 21 9"></polyline>
-                        <line x1="10" y1="14" x2="21" y2="3"></line>
-                      </svg></a></div>
+                          <line x1="10" y1="14" x2="21" y2="3"></line>
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </article>
-</div>
+        <div v-if="activeTab === 2">
+          <div class="grid grid-cols-1 gap-4 pb-32 md:grid-cols-3 md:gap-3 xl:grid-cols-3 xl:gap-3 2xl:gap-5 fade-zoom-in">
+            <div v-for="certificate in certificates" :key="certificate.id">
+              <div class="item-card flex flex-col items-center gap-2 rounded bg-[#1e1e1f] hover:bg-[#282828] border border-[#383838] rounded-xl text-amber-50 md:gap-3 px-5 py-5 lg:px-5 ">
+                <div class="flex h-12 w-12 items-center justify-center p-0 h-full w-full lg:p-0 zoom-in">
+                  <img alt="HTML" loading="lazy" decoding="async" data-nimg="1" class="drop-shadow-xl rounded rounded-xl"
+                    :src="getImageUrl(certificate.imageUrl)">
+                </div>
+                <div class="w-full flex flex-col gap-2 items-center text-sm md:text-base lg:text-lg">
+                  <div class="title-text font-medium text-secondary">{{ certificate.name }}</div>
+                  <div class="w-full text-left text-[10px] text-[#c1c1c1] md:text-xs lg:text-sm">{{ certificate.date }}</div>
+                  <div class="w-full flex justify-end">
+                    <div class="flex cursor-pointer items-end gap-2 text-primary">
+                      <a v-if="certificate.link !== 'null'" :href="certificate.link" target="_blank" rel="noreferrer" title="View certificate" class="transition-all hover:text-accent">
+                        <svg data-v-454f6c7a="" stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="18" width="18" xmlns="http://www.w3.org/2000/svg">
+                          <path data-v-454f6c7a="" d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline data-v-454f6c7a="" points="15 3 21 3 21 9"></polyline><line data-v-454f6c7a="" x1="10" y1="14" x2="21" y2="3"></line></svg>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </article>
+  </div>
 </template>
 
 <script>
@@ -145,15 +169,83 @@ export default {
           tech: 'ipynb',
           github: 'https://github.com/yudhamyn/Banking-Churn-Prediction-Data-Science-Project-IV-GreatEdu/blob/main/Tugas_Mandiri_Data_Science_Project_4_Yudha_Mulyana.ipynb',
           demo: 'null'
+        }
+	],
+      certificates: [
+        { id: 1,
+          name: 'GOOGLE DATA ANALYTICS', 
+          date: 'Issued May 2024', 
+          imageUrl: 'GOOGLEDATAANALYTICS', 
+          link: 'https://coursera.org/share/fca6c4f5bd51f9ead1708b545ed5f121',
         },
-      ]
+        { id: 2,
+          name: 'Data Analysis with Python', 
+          date: 'Issued Jun 2024', 
+          imageUrl: 'Data Analysis with Python', 
+          link: 'https://coursera.org/share/68f58cc7f865229a08c15641250d5080',
+        },
+        { id: 3,
+          name: 'Basic Proficiency in KNIME Analytics', 
+          date: 'Issued May 2024', 
+          imageUrl: 'sertifikatknime', 
+          link: 'https://www.credly.com/badges/8a43c57c-1805-4202-88ae-80139a69253a',
+        },
+        { id: 4,
+          name: 'Database and SQL for Data Science with Python', 
+          date: 'Issued Mar 2024', 
+          imageUrl: 'Database and SQL for Data Science with Python', 
+          link: 'https://coursera.org/share/57edc80cdeb4a52ddb140a3389d34a81',
+        },
+        { id: 5,
+          name: ' Python Project for Data Science', 
+          date: 'Issued Feb 2024', 
+          imageUrl: 'Python Project for Data Science', 
+          link: 'https://coursera.org/share/6ba3030419ec4e3455c0aa8ce628dfa5',
+        },
+        { id: 6,
+          name: 'Python for Data Science, AI & Development', 
+          date: 'Issued Dec 2024', 
+          imageUrl: 'Python for Data Science, AI & Development', 
+          link: 'https://coursera.org/share/35482df6bb12575ee70f4af2d19cc174',
+        },
+        { id: 7,
+          name: 'Bootcamp Microsoft Office Excel Specialist', 
+          date: 'Issued Dec 2024', 
+          imageUrl: 'Bootcamp Microsoft Office Excel Specialist', 
+          link: 'https://drive.google.com/file/d/1nibDgatK5UXSDPjGlG785g2mTJGyWON4/view',
+        },
+        { id: 8,
+          name: 'Data Science Methodology', 
+          date: 'Issued Nov 2024', 
+          imageUrl: 'Data Science Methodology', 
+          link: 'https://coursera.org/share/099622ceca3bb01c1a8c195043f2529a',
+        },
+        { id: 9,
+          name: 'Tools for Data Science', 
+          date: 'Issued Oct 2024',
+          imageUrl: 'Tools for Data Science',
+          link: 'https://coursera.org/share/0cbf91a20b08eedbb1f90012b1e0e601',
+        },
+        { id: 10,
+          name: 'Certificate of Competencies - Kalbe Nutritionals Data Science Virtual Internship Experience Program', 
+          date: 'Issued Sep 2024', 
+          imageUrl: 'sertifikatkalbe', 
+          link: 'https://www.linkedin.com/in/yudha-mulyana/details/certifications/1635542870676/single-media-viewer/?profileId=ACoAADdWofsBjo-DHKqqULGPXCfoeMs0t4im0vw',
+        },
+        { id: 11,
+          name: 'What is Data Science?', 
+          date: 'Issued Aug 2024', 
+          imageUrl: 'What is Data Science?', 
+          link: 'https://coursera.org/share/e29d8d2f79fc4f958ca47521c8af1307',
+        }
+      ],
     };
   },
-  methods: {
+   methods: {
     getImageUrl(imageUrl) {
       const supportedFormats = ['jpg', 'png', 'gif'];
       for (let format of supportedFormats) {
-        let fullPath = `/portofolio/img/${imageUrl}.${format}`;
+        let fullPath = `/img/${imageUrl}.${format}`;
         if (this.imageExists(fullPath)) {
           return fullPath;
         }
@@ -167,10 +259,10 @@ export default {
       return http.status !== 404;
     }
   }
-}
+};
 </script>
 
-<style>
+<style scoped>
 .item-card:hover {
   transition: transform 0.3s ease;
   transform: translateY(-8px);
@@ -190,5 +282,20 @@ svg:hover{
 }
 .fade-zoom-in {
   animation: fadeZoomIn 1s ease-in-out;
+}
+.tabs button {
+  background: none;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  transition: color 0.3s ease;
+}
+
+.tabs button:hover {
+  color: #00aaff;
+}
+
+.tabs button.text-white.border-b-2.border-blue-300 {
+  color: #00aaff;
 }
 </style>
