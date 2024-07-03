467e40318c8c1de21a4faea1ef6d9759cfd22ab0
Yudhamyn <101394691+yudhamyn@users.noreply.github.com>
2024-07-03 13:18:25 +0700

    refactor: Update image source in ArticleView.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <!-- component -->
-  <!-- This is an example component -->
   <div class="max-w-7xl mx-auto flex flex-col relative">
-
     <nav class="max-w-7xl px-5 md:fixed top-0 z-[98] w-screen backdrop-blur-md bg-[#121212] bg-opacity-80">
       <div class="container mx-auto flex flex-wrap items-center justify-between">
         <button @click="redirectToHome" class="flex">
@@ -14,48 +11,60 @@
         <div class="hidden md:flex justify-between items-center w-full md:w-auto md:order-1" id="mobile-menu-3">
           <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
             <li>
-              <router-link to="/"
-                class="fadein-bot text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0"
-                aria-current="page">Home</router-link>
+              <router-link @click.native="setLoading" to="/" class="fadein-bot text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0" aria-current="page">Home</router-link>
             </li>
             <li>
-              <router-link to="/about"
-                class="fadein-bot fadein-1 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">About</router-link>
+              <router-link @click.native="setLoading" to="/about" class="fadein-bot fadein-1 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">About</router-link>
             </li>
             <li>
-              <router-link to="/portfolio"
-                class="fadein-bot fadein-2 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">Portfolio</router-link>
+              <router-link @click.native="setLoading" to="/portfolio" class="fadein-bot fadein-2 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">Portfolio</router-link>
             </li>
             <li>
-              <router-link to="/blog"
-                class="fadein-bot fadein-3 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0 blog">Experiences</router-link>
+              <router-link @click.native="setLoading" to="/blog" class="fadein-bot fadein-3 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0 blog">Experiences</router-link>
             </li>
           </ul>
         </div>
       </div>
     </nav>
-
     <div class="md:mt-[100px]">
       <router-view />
     </div>
+    <footer class="block md:hidden fixed bottom-0 left-0 right-0 rounded-t-3xl border border-[#383838] bg-[#121212] bg-opacity-80 backdrop-blur-md backdrop-opacity-90">
+      <nav class="flex justify-around py-4 text-xs">
+        <router-link @click.native="setLoading" to="/" class="text-gray-300 hover:text-white">Home</router-link>
+        <router-link @click.native="setLoading" to="/about" class="text-gray-300 hover:text-white">About</router-link>
+        <router-link @click.native="setLoading" to="/portfolio" class="text-gray-300 hover:text-white">Portfolio</router-link>
+        <router-link @click.native="setLoading" to="/blog" class="text-gray-300 hover:text-white">Experiences</router-link>
+      </nav>
+    </footer>
+    <div v-if="isLoading" class="loading-overlay">
+      <div class="spinner"></div>
+    </div>
   </div>
-  <footer class="block md:hidden fixed bottom-0 left-0 right-0 rounded-t-3xl border border-[#383838] bg-[#121212] bg-opacity-80 backdrop-blur-md backdrop-opacity-90">
-  <nav class="flex justify-around py-4 text-xs">
-    <router-link to="/" class="text-gray-300 hover:text-white">Home</router-link>
-    <router-link to="/about" class="text-gray-300 hover:text-white">About</router-link>
-    <router-link to="/portfolio" class="text-gray-300 hover:text-white">Portfolio</router-link>
-    <router-link to="/blog" class="text-gray-300 hover:text-white">Experiences</router-link>
-  </nav>
-</footer>
 </template>
+
 
 <script>
 export default {
-  methods: {
-    redirectToHome() {
-      this.$router.push('/')
+  data() {
+    return {
+      isLoading: false
+    };
+  },
+  watch: {
+    '$route'() {
+      this.isLoading = false;
     }
   },
+  methods: {
+    setLoading() {
+      this.isLoading = true;
+    },
+    redirectToHome() {
+      this.setLoading();
+      this.$router.push('/');
+    }
+  }
 }
 </script>
 
@@ -126,7 +135,6 @@ nav a.router-link-exact-active:hover {
   color: white;
 }
 
-
 @keyframes fadeInLeft {
   0% {
     opacity: 0;
@@ -138,4 +146,28 @@ nav a.router-link-exact-active:hover {
   }
 }
 
+.loading-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1000;
+}
+.spinner {
+  border: 0.25rem solid #f3f3f3;
+  border-top: 0.25rem solid #3498db;
+  border-radius: 50%;
+  width: 2rem;
+  height: 2rem;
+  animation: spin 1s linear infinite;
+}
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
 </style>
