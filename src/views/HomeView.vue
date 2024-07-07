b4e2832fafcebd67889251ce06cfce88c6db5257
Yudhamyn <101394691+yudhamyn@users.noreply.github.com>
2024-07-07 23:17:56 +0700

    refactor: Update download button styling and animation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,13 +11,13 @@
       <p class="text-white pr-4 fade-in-from-left text-sm md:text-base">Welcome to My personal website. <span class="wave">👋🏼</span></p>
       <br>
       <button
-        class="fadein-bot fade-500 flex items-center py-2 px-4 mx-auto text-xs md:text-sm font-medium rounded-lg border transition duration-300 md:py-2.5 md:px-5 md:mx-0 text-blue-200 border-blue-200 hover:bg-blue-200 hover:bg-opacity-10 bg-transparent focus:outline-none w-fit mb-16 md:mb-0"
+        class="download-button fadein-bot fade-500 flex items-center py-2 px-4 mx-auto text-xs md:text-sm font-medium rounded-lg border transition duration-300 md:py-2.5 md:px-5 md:mx-0 text-blue-200 border-blue-200 bg-transparent focus:outline-none w-fit mb-16 md:mb-0"
         @click="downloadResume">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2 w-4 h-4">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2 w-4 h-4 svg-icon">
           <path fill-rule="evenodd" d="M5.625 1.5H9a3.75 3.75 0 013.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 013.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 01-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875zm5.845 17.03a.75.75 0 001.06 0l3-3a.75.75 0 10-1.06-1.06l-1.72 1.72V12a.75.75 0 00-1.5 0v4.19l-1.72-1.72a.75.75 0 00-1.06 1.06l3 3z" clip-rule="evenodd"></path>
           <path d="M14.25 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0016.5 7.5h-1.875a.375.375 0 01-.375-.375V5.25z"></path>
         </svg>
-        Download Resume
+        <span class="button-text">Download Resume</span>
       </button>
     </div>
     <div class="flex justify-center md:justify-start fadein-right">
@@ -88,7 +88,6 @@ export default {
   }
 }
 </script>
-
 
 <style>
 body {
@@ -241,5 +240,17 @@ body {
     display: block;
     margin-bottom: 16px; /* Ensure the margin-bottom is also applied in mobile view */
   }
+}
+
+.download-button:hover {
+  background-color: #00fbff;
+  color: #000000; /* Change text color to black */
+  transform: scale(1.05); /* Slightly increase the button size */
+  transition: background-color 0.3s, color 0.3s, transform 0.3s; /* Smooth transition */
+}
+
+.download-button:hover .svg-icon, .download-button:hover .button-text {
+  transform: scale(1.1);
+  transition: transform 0.1s;
 }
 </style>
